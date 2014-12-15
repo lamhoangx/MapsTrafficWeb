ffe9e8898486c116dc027b7eac1dd8f5b0910cce
lamhx393 <lamhx393@gmail.com>
2014-12-16 01:07:52 +0700

    update post

--- MapTraffic/www/mapstraffic.js
+++ MapTraffic/www/mapstraffic.js
@@ -1,4 +1,3 @@
-
 var server = 'http://128.199.139.117';
 var dataJsonStreet;
 var directionDisplay;
@@ -11,22 +10,24 @@ var currentLocation = 0;
 var isCheckGetLocationStart = 0;
 var isCheckGetLocationEnd = 0;
 
-function onDeviceReady(){
-	alert('ready');
-    navigator.geolocation.watchPosition(onSuccessPos, onError, {timeout: 10000, enableHighAccuracy: true});
-    
+function onDeviceReady() {
+    alert('ready');
+    navigator.geolocation.watchPosition(onSuccessPos, onError, {
+        timeout: 10000,
+        enableHighAccuracy: true
+    });
 }
-function onSuccessPos(position){
-    currentLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+
+function onSuccessPos(position) {
+    currentLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 }
 
 function onError(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
+    alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
 }
 
 function initialize() {
-	document.addEventListener("deviceready", onDeviceReady, false);
+    document.addEventListener("deviceready", onDeviceReady, false);
     directionsDisplay = new google.maps.DirectionsRenderer();
     var mapOptions = {
         center: new google.maps.LatLng(10.87178429, 106.80741102),
@@ -39,27 +40,25 @@ function initialize() {
     var myLocation = document.getElementById('my_location');
     myLocation.style.display = 'block';
     map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(myLocation);
-
     google.maps.event.addListener(map, 'click', function(event) {
         console.log('Click Map');
-            var lat = event.latLng.lat();
-            var lng = event.latLng.lng();
-        if(isCheckGetLocation===1){
+        var lat = event.latLng.lat();
+        var lng = event.latLng.lng();
+        if (isCheckGetLocation === 1) {
             $('#latitude_location').val(lat);
             $('#longitude_location').val(lng);
             window.history.back();
             isCheckGetLocation = 0;
         }
-        if(isCheckGetLocationStart===1){
-        	$('#point_start').val(lat+","+lng);
-        	window.history.back();
-        	isCheckGetLocationStart = 0;
+        if (isCheckGetLocationStart === 1) {
+            $('#point_start').val(lat + "," + lng);
+            window.history.back();
+            isCheckGetLocationStart = 0;
         }
-        if(isCheckGetLocationEnd===1){
-        	$('#point_end').val(lat+","+lng);
-        	window.history.back();
-        	isCheckGetLocationEnd = 0;
-
+        if (isCheckGetLocationEnd === 1) {
+            $('#point_end').val(lat + "," + lng);
+            window.history.back();
+            isCheckGetLocationEnd = 0;
         }
     });
 }
@@ -96,8 +95,7 @@ function calcRoute() {
                 dataJsonStreet += "{\"latitude\":\"" + route.overview_path[i].k + "\",\"longitude\":\"" + route.overview_path[i].D + "\"},";
                 arrLatLng.push(new google.maps.LatLng(route.overview_path[i].k, route.overview_path[i].D));
             }
-
-		/*
+            /*
              flightPath = new google.maps.Polyline({
               path: arrLatLng,
               geodesic: true,
@@ -108,7 +106,7 @@ function calcRoute() {
 
             // Dùng hàm setMap để gắn vào bản đồ
             flightPath.setMap(map);
-		*/
+        */
             var lengthDataJson = dataJsonStreet.length;
             dataJsonStreet = dataJsonStreet.substring(0, lengthDataJson - 1);
             dataJsonStreet += ']}';
@@ -126,75 +124,83 @@ function calcRoute() {
 
 function postDataToServer(data) {
     console.log('post to server');
-    $.post(server, data, function() {
-        console.log("success");
-    }).done(function() {
-        console.log("second success");
+    // $.post(server, data, function() {
+    //     console.log("success");
+    // }).done(function() {
+    //     console.log("second success");
+    // }).fail(function() {
+    //     console.log("error");
+    // }).always(function() {
+    //     console.log("finished");
+    // });
+    // console.log('finish post');
+    $.ajax({
+    url: server,
+    data: data
+    }).done(function(data) {
+        alert("Success: " + data.param1);
     }).fail(function() {
-        console.log("error");
-    }).always(function() {
-        console.log("finished");
+        alert("Error");
     });
-
-    console.log('finish post');
 }
 
-
-function postLocationToServer(data){
+function postLocationToServer(data) {
     console.log('Post Location');
-    $.post(server,data,function() {
-        console.log("success");
-    }).done(function() {
-        console.log("second success");
+    // $.post(server, data, function() {
+    //     console.log("success");
+    // }).done(function() {
+    //     console.log("second success");
+    // }).fail(function() {
+    //     console.log("error");
+    // }).always(function() {
+    //     console.log("finished");
+    // });
+
+    $.ajax({
+    url: server,
+    data: data
+    }).done(function(data) {
+        alert("Success: " + data.param1);
     }).fail(function() {
-        console.log("error");
-    }).always(function() {
-        console.log("finished");
+        alert("Error");
     });
 }
-
 $("#btn_get_street_ok").click(function() {
     //console.log("Post Data");
     calcRoute();
 });
-
-$("#get_location_from_map").click(function(){
+$("#get_location_from_map").click(function() {
     isCheckGetLocation = 1;
-	isCheckGetLocationEnd = 0;
-	isCheckGetLocationStart = 0;
+    isCheckGetLocationEnd = 0;
+    isCheckGetLocationStart = 0;
 });
-
 $("#btn_location_ok_post").click(function() {
     console.log("Add Data");
     var loc = $('#address_location').val();
     var lat = $('#latitude_location').val();
     var lng = $('#longitude_location').val();
     var err = $('#error_location').val();
-    if(loc!=="" && lat !=="" && lng!=="" && err !==""){
-        var strJson = '{\"diadiem\":\"'+loc+'\",\"locationX\":'+lat+',\"locationY\":'+lng+',\"loivipham\":\"'+err+'\"}';
+    if (loc !== "" && lat !== "" && lng !== "" && err !== "") {
+        var strJson = '{\"diadiem\":\"' + loc + '\",\"locationX\":' + lat + ',\"locationY\":' + lng + ',\"loivipham\":\"' + err + '\"}';
         // var strJson = '{\"diadiem\":\"'+loc+'\",\"locationX\":\"'+lat+'\",\"locationY\":\"'+lng+'\",\"loivipham\":\"'+err+'\"}';
         postDataToServer(strJson);
         alert("Thank for submit!");
         console.log(strJson);
     }
 });
-
-$("#my_location").click(function(){
-	alert(currentLocation.toString());
+$("#my_location").click(function() {
+    alert(currentLocation.toString());
     map.setCenter(currentLocation);
 });
-
-$("#get_location_start_from_map").click(function(){
-	isCheckGetLocation = 0;
-	isCheckGetLocationEnd = 0;
-	isCheckGetLocationStart = 1;
-
+$("#get_location_start_from_map").click(function() {
+    isCheckGetLocation = 0;
+    isCheckGetLocationEnd = 0;
+    isCheckGetLocationStart = 1;
 });
-
-$("#get_location_end_from_map").click(function(){
-	isCheckGetLocation = 0;
-	isCheckGetLocationEnd = 1;
-	isCheckGetLocationStart = 0;
+$("#get_location_end_from_map").click(function() {
+    isCheckGetLocation = 0;
+    isCheckGetLocationEnd = 1;
+    isCheckGetLocationStart = 0;
 });
 
 // $( document ).on( "pageinit", "#mainPager", function() {
@@ -211,5 +217,4 @@ $("#get_location_end_from_map").click(function(){
 //         }
 //     });
 // });
-
 google.maps.event.addDomListener(window, 'load', initialize);
