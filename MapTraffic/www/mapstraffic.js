3ea01ae8219378fa6fe2f503cd335b252124c0db
lamhx393 <lamhx393@gmail.com>
2014-12-16 00:53:34 +0700

    update func into slide menu

--- MapTraffic/www/mapstraffic.js
+++ MapTraffic/www/mapstraffic.js
@@ -197,19 +197,19 @@ $("#get_location_end_from_map").click(function(){
 	isCheckGetLocationStart = 0;
 });
 
-$( document ).on( "pageinit", "#mainPager", function() {
-    $( document ).on( "swipeleft swiperight", "#mainPager", function( e ) {
-        // We check if there is no open panel on the page because otherwise
-        // a swipe to close the left panel would also open the right panel (and v.v.).
-        // We do this by checking the data that the framework stores on the page element (panel: open).
-        if ( $.mobile.activePage.jqmData( "panel" ) !== "open" ) {
-            if ( e.type === "swipeleft"  ) {
-                $( "#right-panel" ).panel( "open" );
-            } else if ( e.type === "swiperight" ) {
-                $( "#left-panel" ).panel( "open" );
-            }
-        }
-    });
-});
+// $( document ).on( "pageinit", "#mainPager", function() {
+//     $( document ).on( "swipeleft swiperight", "#mainPager", function( e ) {
+//         // We check if there is no open panel on the page because otherwise
+//         // a swipe to close the left panel would also open the right panel (and v.v.).
+//         // We do this by checking the data that the framework stores on the page element (panel: open).
+//         if ( $.mobile.activePage.jqmData( "panel" ) !== "open" ) {
+//             if ( e.type === "swipeleft"  ) {
+//                 $( "#right-panel" ).panel( "open" );
+//             } else if ( e.type === "swiperight" ) {
+//                 $( "#left-panel" ).panel( "open" );
+//             }
+//         }
+//     });
+// });
 
 google.maps.event.addDomListener(window, 'load', initialize);
