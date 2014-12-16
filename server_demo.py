e71b1e6fef81353c14866d229235bd9fd5bc2723
xxvicon <xxvicon@gmail.com>
2014-12-16 15:01:39 +0700

    Fix Send Response to Client
    Add Header Access-Control-Allow-Origin: *

--- server_demo.py
+++ server_demo.py
@@ -1,10 +1,10 @@
-
-
 #!/usr/bin/python
 import SocketServer
 import json
 import mysql.connector
 import time
+import decimal
+import math
 from mysql.connector import errorcode
 
 class MyTCPServer(SocketServer.ThreadingTCPServer):
@@ -54,8 +54,8 @@ def parserHeader(data):
 		method = "GET"
 	HTTPversion = datalines[0].split(" ")[2]
 	mydict = {};
-	mydict['method'] = method
-	mydict['HTTPversion'] = HTTPversion
+	mydict["method"] = method
+	mydict["HTTPversion"] = HTTPversion
 	for i in xrange(1,len(datalines)):
 		requestfield = datalines[i].split(": ") 
 		mydict[requestfield[0]] = requestfield[1]
@@ -73,61 +73,91 @@ def parserData(data):
 	
 	
 def insertFound(data):
-	data_location = (data['diadiem'],"%.15f" % float(data['locationX']), "%.15f" %float(data['locationY']), data['loivipham'])
+	data_location = (data["diadiem"],str(decimal.Decimal(data["locationX"])), str(decimal.Decimal(data["locationY"])), data["loivipham"])
 	add_location = ("INSERT INTO tb_location (diadiem, locationX, locationY, loivipham) VALUES (%s, %s, %s, %s)")
 	rows = selectDB()
 	for row in rows:
-		print row,data['locationX'],data['locationY']
-		if(row[2]==float(data['locationX']) and row[3]==float(data['locationY'])):
+		print row,data["locationX"],data["locationY"]
+		if(row[2]==float(data["locationX"]) and row[3]==float(data["locationY"])):
 			data = {}
-			data['loivipham'] = row[4]
-			data['locationX'] = row[2]
-			data['locationY'] = row[3]
-			data['diadiem'] = row[1]
-			data["Error"]="Location "+"%.15f" % data['locationX']+","+"%.15f" %data['locationY']+" exist in Database!"
+			data["loivipham"] = row[4]
+			data["locationX"] = row[2]
+			data["locationY"] = row[3]
+			data["diadiem"] = row[1]
+			data["Error"]="Location "+str(decimal.Decimal(data["locationX"]))+","+str(decimal.Decimal(data["locationY"]))+" exist in Database!"
 			data["Message"]="Insert Fail"
 			return data
 	insert2DB(data_location,add_location)
 	data["Message"] = "Insert Success"
 	return data
+	
+def findFound2Point(data,founds):
+	N = decimal.Decimal("0.00015")
+	dlkc = decimal.Decimal("0.00015") # Do lech khoang cach 30m
+	finalFound = []
+	allPoints = data["street"]
+	for i in xrange(1,len(allPoints)):
+		x1 = decimal.Decimal(allPoints[i-1]["latitude"])
+		x2 = decimal.Decimal(allPoints[i]["latitude"])
+		y1 = decimal.Decimal(allPoints[i-1]["longitude"])
+		y2 = decimal.Decimal(allPoints[i]["longitude"])
+		XMIN = min(x1,x2)
+		XMAX = max(x1,x2)
+		YMIN = min(y1,y2)
+		YMAX = max(y1,y2)
+		a = x2-x1
+		b = y1-y2
+		c = x1*(x1-x2)+y1*(y2-y1)
+		for f in founds:
+			if  f[2] < XMAX+N and f[2] > XMIN-N and f[3] < YMAX+N and f[3] > YMIN-N:
+				kc = decimal.Decimal(abs(a*decimal.Decimal(f[2])+b*decimal.Decimal(f[3])+c)/decimal.Decimal(math.sqrt(a*a+b*b)))
+				print x1,y1,x2,y2,f[2],f[3],kc
+				if kc < dlkc:
+					finalFound.append(f)
+	return finalFound
 def findFound(data):
 	# {"street":[{"latitude":"10.849710000000002","longitude":"106.77166000000001"},{"latitude":"10.84964","longitude":"106.77213"},{"latitude":"10.8496","longitude":"106.77285"},{"latitude":"10.849580000000001","longitude":"106.77337000000001"},{"latitude":"10.84952","longitude":"106.77351"},{"latitude":"10.849400000000001","longitude":"106.77372000000001"},{"latitude":"10.849260000000001","longitude":"106.77391000000001"},{"latitude":"10.84903","longitude":"106.77415"},{"latitude":"10.849480000000002","longitude":"106.77458000000001"},{"latitude":"10.850280000000001","longitude":"106.77550000000001"}]}
 	# print type(data["street"])
 	# print data["street"]
 	# print "\n\n\n\n",data["street"][0],type(data["street"][0])
-	XMAX = float(data["street"][0]["latitude"])
-	XMIN = float(data["street"][0]["latitude"])
-	YMAX = float(data["street"][0]["longitude"])
-	YMIN = float(data["street"][0]["longitude"])
+	XMAX = decimal.Decimal(data["street"][0]["latitude"])
+	XMIN = decimal.Decimal(data["street"][0]["latitude"])
+	YMAX = decimal.Decimal(data["street"][0]["longitude"])
+	YMIN = decimal.Decimal(data["street"][0]["longitude"])
 	for itm in data["street"]:
-		X = float(itm["latitude"])
+		X = decimal.Decimal(itm["latitude"])
 		if  X > XMAX:
 			XMAX = X
 		elif X < XMIN:
 			XMIN = X
-		Y = float(itm["longitude"])
+		Y = decimal.Decimal(itm["longitude"])
 		if Y > YMAX:
 			YMAX = Y
 		elif Y < YMIN:
 			YMIN = Y
-	print "X:",XMAX,XMIN
-	print "Y:",YMAX,YMIN
-	print selectDB()
-	founds = selectDB("where locationX<"+str(XMAX)+" and locationX>"+str(XMIN)+" and locationY<"+str(YMAX)+" and locationY>"+str(YMIN))
-	print founds,type(founds)
+	# print "X:",XMAX,XMIN
+	# print "Y:",YMAX,YMIN
+	# print selectDB()
+	founds = selectDB("where locationX<"+str(decimal.Decimal(XMAX))+" and locationX>"+str(decimal.Decimal(XMIN))+" and locationY<"+str(decimal.Decimal(YMAX))+" and locationY>"+str(decimal.Decimal(YMIN)))
+	# print founds,type(founds)
 	if len(founds)>0:
+		finalFound = findFound2Point(data,founds)
+		print "finalFound",finalFound
 		listFound = []
-		data = {}
-		for itm in founds:
+		founddata = {}
+		for itm in finalFound:
 			found = {}
-			found['loivipham'] = itm[4]
-			found['locationX'] = itm[2]
-			found['locationY'] = itm[3]
-			found['diadiem'] = itm[1]
+			found["loivipham"] = itm[4]
+			found["locationX"] = str(itm[2])
+			found["locationY"] = str(itm[3])
+			found["diadiem"] = itm[1]
 			listFound.append(found)
-		data["Message"]= "Found"
-		data["AllFound"] = listFound
-	return data
+		founddata["Message"]= "Found"
+		founddata["AllFound"] = listFound
+	else:
+		founddata = {}
+		founddata["Message"]= "No Found"
+	return founddata
 def insert2DB(data,add):
 	cursor.execute(add, data)
 	cnx.commit()
@@ -150,20 +180,18 @@ class MyTCPServerHandler(SocketServer.BaseRequestHandler):
 			print "Exception wile receiving message: ", e
 			data2send["Message"]=str(e)
 			print data2send
-			self.request.sendall(json.dumps({'return':e}))
+			self.request.sendall(json.dumps({"return":e}))
 
 		print self.request.getpeername()
 		mydict = parserHeader(data)
-		if(mydict['method']=="GET"):
-			data2send['Message'] = "Wellcome Demo Mobile Application"
-			data2send['ip'] = self.request.getpeername()
+		if(mydict["method"]=="GET"):
+			data2send["Message"] = "Wellcome Demo Mobile Application"
+			data2send["ip"] = self.request.getpeername()
 		else:
 			data2send = parserData(data)
-		print "Send to client:",data2send
+		print "Send to client:","HTTP/1.1\nAccess-Control-Allow-Origin: *\r\n\r\n"+json.dumps(data2send)
 		print "="*40
-		self.request.sendall(json.dumps(data2send))
-		
-
+		self.request.sendall("HTTP/1.1\nAccess-Control-Allow-Origin: *\r\n\r\n"+json.dumps(data2send))
 try:
 	cnx = mysql.connector.connect(user='root', password='cs332f12', database='demomobile')
 except mysql.connector.Error as err:
@@ -177,5 +205,5 @@ except mysql.connector.Error as err:
 		exit();
 cursor = cnx.cursor()
 
-server = MyTCPServer(('0.0.0.0', 13373), MyTCPServerHandler)
+server = MyTCPServer(('0.0.0.0', 80), MyTCPServerHandler)
 server.serve_forever()
